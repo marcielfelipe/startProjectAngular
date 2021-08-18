8d58c511ea1955f9ca7961b875d0dffde083f1fc
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2021-08-18 14:52:05 -0300

    ajust app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,16 @@
-import { ProductAlertsComponent } from './molecules/product-alerts/product-alerts.component';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ButtonComponent } from './atoms/button/button.component';
-import { ButtonIconComponent } from './molecules/button-icon/button-icon.component';
-import { CartItemComponent } from './molecules/cart-item/cart-item.component';
-import { FormCheckoutComponent } from './molecules/form-checkout/form-checkout.component';
-import { ProductComponent } from './molecules/product/product.component';
-import { CartComponent } from './templates/cart/cart.component';
-import { ProductDetailsComponent } from './templates/product-details/product-details.component';
-import { ProductListComponent } from './templates/product-list/product-list.component';
-import { ShippingComponent } from './templates/shipping/shipping.component';
-import { TopBarComponent } from './templates/top-bar/top-bar.component';
-import { CommonModule } from '@angular/common';
-import { AtomsModule } from './atoms/atoms.module';
-import { MoleculesModule } from './molecules/molecules.module';
 import { TemplatesModule } from './templates/templates.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // ButtonComponent,
-    // ButtonIconComponent,
-    // CartItemComponent,
-    // FormCheckoutComponent,
-    // ProductComponent,
-    // ProductAlertsComponent,
-    // CartComponent,
-    // ProductDetailsComponent,
-    // ProductListComponent,
-    // ShippingComponent,
-    // TopBarComponent
   ],
   imports: [
     BrowserModule,
